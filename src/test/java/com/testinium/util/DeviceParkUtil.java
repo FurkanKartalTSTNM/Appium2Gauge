0f8ce378c6f02291a978632c38db1042c2748f20
Furkan Kartal <Furkan.kartal@testinium.com>
2025-02-20 01:29:56 +0300

    Change enviroment

--- src/test/java/com/testinium/util/DeviceParkUtil.java
+++ src/test/java/com/testinium/util/DeviceParkUtil.java
@@ -15,7 +15,7 @@ public class DeviceParkUtil {
     public static void setDeviceParkOptions(DesiredCapabilities capabilities) {
         HashMap<String, Object> deviceParkOptions = new HashMap<>();
         deviceParkOptions.put(SESSION_ID, com.testinium.util.Environment.sessionId);
-        deviceParkOptions.put(APPIUM_VERSION, Environment.sessionId);
+        deviceParkOptions.put(APPIUM_VERSION, Environment.appiumVersion);
         capabilities.setCapability(DP_OPTIONS, deviceParkOptions);
     }
 }
