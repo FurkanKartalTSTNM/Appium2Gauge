b8dab0e25150f21b71fea6992aacaeb8559dd071
Furkan Kartal <Furkan.kartal@testinium.com>
2025-02-25 17:44:37 +0300

    isAcceptable

--- src/test/java/com/testinium/util/CommandUtil.java
+++ src/test/java/com/testinium/util/CommandUtil.java
@@ -1,5 +1,8 @@
 package com.testinium.util;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,13 +11,12 @@ import static com.testinium.util.Constants.SESSION;
 public class CommandUtil {
 
     public static Boolean isAcceptable(String pathInfo, List<String> commands) {
-        List<String> pathSegments = Arrays.asList(pathInfo.split("/"));
-
-        if (pathSegments.size() >= 4 && SESSION.equals(pathSegments.get(1))) {
-            String commandPath = String.join("/", pathSegments.subList(3, pathSegments.size()));
-            return !commands.contains(commandPath) && !commands.contains(pathSegments.getLast());
+        List<String> path = new ArrayList<>(Arrays.asList(pathInfo.split("/")));
+        if (path.size() >= 3 && path.get(1).equals(SESSION) && path.size() > 3) {
+            path = path.subList(3, path.size());
+            String commandPath = StringUtils.join(path, "/");
+            return !(commands.contains(commandPath) || commands.contains(path.get(path.size() - 1)));
         }
-
         return false;
     }
 
